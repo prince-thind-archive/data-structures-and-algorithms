bf76947386027a4a7c8218048e4429a670326c92
Prince Thind <Princethind799@gmail.com>
2022-02-22 15:43:21 +0530

    implement iterator for linked list

--- data-structures/linkedList.js
+++ data-structures/linkedList.js
@@ -1,22 +1,51 @@
 class LinkedList {
-  #arr;
   constructor() {
-    this.#arr = [];
     this.head = null;
     this.tail = null;
   }
+
   append(element) {
-    const node = { data:element, pointer: null };
+    const node = { data: element, pointer: null };
+
+    // if head doesnt' exist
     if (this.head == null) {
       this.head = node;
     }
+
+    //if tail doesn't exist
     if (this.tail == null) {
       this.tail = node;
-    } else {
-      this.tail.pointer = node;
-      this.tail = node;
+      return;
     }
-    this.#arr.push(node);
+
+    //if tail exists update pointers;
+    this.tail.pointer = node;
+    this.tail = node;
+  }
+
+  [Symbol.iterator]() {
+    // list is empty do nothing
+    if (this.head == null) {
+      return {
+        next() {
+          return { done: true };
+        },
+      };
+    }
+
+    //list contains items
+    return {
+      current: this.head,
+      next() {
+        if (this.current != null) {
+          const obj = { done: false, value: this.current.data };
+          this.current = this.current.pointer;
+          return obj;
+        } else {
+          return { done: true };
+        }
+      },
+    };
   }
 }
 
@@ -24,7 +53,7 @@ const list = new LinkedList();
 for (let i = 0; i < 25; i++) {
   list.append(i);
 }
-for (let i = list.head; i.pointer != null; i = i.pointer) {
-  console.log(i.data);
+
+for(const i of list){
+  console.log(i)
 }
-console.log(list);
